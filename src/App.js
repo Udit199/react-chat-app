ac21c25b887a4bef6606dbbabbf4b8a53b23a4b6
udit narayan dixit <81948798+Udit199@users.noreply.github.com>
2023-06-24 18:08:32 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ChatDashboard from "./ChatDashboard";
+
+
+class App extends Component {
+    state = {
+        messages: [
+            {username: 'Udit', text: 'Hi, Rahul!'},
+            {username: 'Udit', text: 'How are you?'},
+            {username: 'Rahul', text: 'Hi, Udit! Good, you?'},
+        ],
+        users: [{username: 'Udit'}, {username: 'Rahul'}]
+    };
+
+    updateMessageListing = (message) => {
+        let update_messages_list = this.state.messages
+        update_messages_list.push(message)
+
+        this.setState({
+            messages: update_messages_list,
+        });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">ReactND - Coding Practice</h1>
+                </header>
+                <div className="container">
+                    {
+                        this.state.users.map(user => (
+                            <ChatDashboard
+                                key={user.username}
+                                user={user}
+                                updateMessages={this.updateMessageListing}
+                                messages={this.state.messages}
+                            />
+                        ))
+                    }
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
